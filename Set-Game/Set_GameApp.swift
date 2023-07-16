60a007639b81976dbe9873e930650f2ee278b690
DevHarleyA <aharleydev@gmail.com>
2023-07-16 12:13:55 -0400

    Initial Commit

---
+++ Set-Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set-Game
+//
+//  Created by Alexandra Harley on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
